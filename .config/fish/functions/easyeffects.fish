ff6f2b499bfe13b4afb4d85ea091132b324f0f72
MNolan147 <mjnolan32@gmail.com>
2022-04-12 13:27:26 +1000

    Added sway environment config files from .config
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

---
+++ .config/fish/functions/easyeffects.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function easyeffects --wraps='flatpak run com.github.wwmm.easyeffects' --description 'alias easyeffects=flatpak run com.github.wwmm.easyeffects'
+  flatpak run com.github.wwmm.easyeffects $argv;
+end
